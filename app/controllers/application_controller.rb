9917e160d64de1d41825b42d226d6a9689273b11
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-19 18:03:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
+    user = User.new(:username => params[:username], :password => params[:password])
 
+  if user.save
+      redirect "/login"
+  else
+      redirect "/failure"
   end
+end
+
 
   get '/account' do
     @user = User.find(session[:user_id])
@@ -32,7 +38,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-  end 
+  end
 
 
   get "/failure" do
